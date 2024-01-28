1975c33e9ce0754fcd478703452c8be983d6e51a
WeiMin-Coding <1772429411@qq.com>
2024-01-28 08:39:06 +0800

    String

---
+++ src/chapter01_Variable/stringDemo/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var c1 byte = 'a'
+	var c2 byte = '0'
+	fmt.Println("c1 =", c1)
+	fmt.Println("c2 =", c2)
+	fmt.Printf("c1=%c c2=%c\n", c1, c2)
+
+	var c3 byte = '1'
+	fmt.Println(c3)
+}
